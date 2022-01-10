db4fc00645fb4f98c12be00b8a5c0d6c28734149
Mrrshaw <thimmy.stenlund@gmail.com>
2022-01-10 12:08:15 +0100

    Add files via upload

---
+++ Connect.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace _2DV513A3
+{
+    public class Connect
+    {
+        public void menuItem1()
+        {
+            string connectString;
+            SqlConnection sqlConn;
+            connectString = @"Data Source=localhost;Database=Assignment3;Initial catalog = ;User ID=usera3;Password=hemligt";
+            sqlConn = new SqlConnection(connectString);
+            sqlConn.Open();
+            Console.WriteLine("");
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, output = "";
+
+            sql = @"Select name, label, title FROM [Assignment3].[dbo].[Artist] AS art INNER JOIN [Assignment3].[dbo].[Song] AS song ON art.name = song.artist";
+
+            command  = new SqlCommand(sql, sqlConn);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                output = output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + " - " +  dataReader.GetValue(2) + "\n";
+            } 
+            
+            Console.WriteLine(output);
+            Console.WriteLine();
+
+            sqlConn.Close();
+        }
+
+        public void menuItem2()
+        {
+            string connectString;
+            SqlConnection sqlConn;
+            connectString = @"Data Source=localhost;Database=Assignment3;Initial catalog = ;User ID=usera3;Password=hemligt";
+            sqlConn = new SqlConnection(connectString);
+            sqlConn.Open();
+            Console.WriteLine("");
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, output = "";
+
+            sql = @"Select name, label, earnings FROM [Assignment3].[dbo].[Artist] AS art INNER JOIN [Assignment3].[dbo].[Label] AS label ON label.organisation = art.label";
+
+            command = new SqlCommand(sql, sqlConn);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                output = output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + " - " + dataReader.GetValue(2) + "\n";
+            }
+
+            Console.WriteLine(output);
+            Console.WriteLine();
+
+            sqlConn.Close();
+        }
+
+        public void menuItem3()
+        {
+            string connectString;
+            SqlConnection sqlConn;
+            connectString = @"Data Source=localhost;Database=Assignment3;Initial catalog = ;User ID=usera3;Password=hemligt";
+            sqlConn = new SqlConnection(connectString);
+            sqlConn.Open();
+            Console.WriteLine("");
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, output = "";
+
+            sql = @"SELECT name, label, title,FameFactor FROM [Assignment3].[dbo].[artistdata]";
+
+            command = new SqlCommand(sql, sqlConn);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                output = output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + " - " + dataReader.GetValue(2) + " - " + dataReader.GetValue(3) + "\n";
+            }
+
+            Console.WriteLine(output);
+            Console.WriteLine();
+
+            sqlConn.Close();
+        }
+
+        public void menuItem4()
+        {
+            string connectString;
+            SqlConnection sqlConn;
+            connectString = @"Data Source=localhost;Database=Assignment3;Initial catalog = ;User ID=usera3;Password=hemligt";
+            sqlConn = new SqlConnection(connectString);
+            sqlConn.Open();
+            Console.WriteLine("");
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, output = "";
+
+            sql = @"SELECT name, famefactor, MAX(famefactor) AS max_famefactor FROM [Assignment3].[dbo].[Artist] GROUP BY famefactor, name ORDER BY famefactor DESC";
+            command = new SqlCommand(sql, sqlConn);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                output = output + dataReader.GetValue(0) + "\n";
+            }
+
+            Console.WriteLine(output);
+            Console.WriteLine();
+
+            sqlConn.Close();
+        }
+
+        public void menuItem5()
+        {
+            string connectString;
+            SqlConnection sqlConn;
+            connectString = @"Data Source=localhost;Database=Assignment3;Initial catalog = ;User ID=usera3;Password=hemligt";
+            sqlConn = new SqlConnection(connectString);
+            sqlConn.Open();
+            Console.WriteLine("");
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, output = "";
+
+            sql = @"SELECT SUM(earnings) AS sumOfEarnings FROM [Assignment3].[dbo].[Label] ORDER BY sumOfEarnings DESC";
+            command = new SqlCommand(sql, sqlConn);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                output = output + dataReader.GetValue(0) + "\n";
+            }
+
+            Console.WriteLine(output);
+            Console.WriteLine();
+
+            sqlConn.Close();
+        }
+
+        public void menuItem6()
+        {
+            string connectString;
+            SqlConnection sqlConn;
+            connectString = @"Data Source=localhost;Database=Assignment3;Initial catalog = ;User ID=usera3;Password=hemligt";
+            sqlConn = new SqlConnection(connectString);
+            sqlConn.Open();
+            Console.WriteLine("");
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, output = "";
+
+            sql = @"SELECT * FROM [Assignment3].[dbo].[Artist] WHERE famefactor >= 5 ORDER BY famefactor DESC";
+            command = new SqlCommand(sql, sqlConn);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                output = output + dataReader.GetValue(0) + "\n";
+            }
+
+            Console.WriteLine(output);
+            Console.WriteLine();
+
+            sqlConn.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _2DV513A3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isValidMenuItem = false;
+
+            Connect connect = new Connect();
+            Console.WriteLine("Welcome to the Artist & Song compository!");
+            Console.WriteLine("What do you want to do today? (Enter Setup or a number)");
+            Console.WriteLine("Setup. Setup database (Tables)");
+            Console.WriteLine("1: Artist, Label & Song" + "\n" + 
+                "2: Artist, Label & Earnings (for the artist's label,  in 1000$'s)" + "\n" + 
+                "3: See view for data from Artist, Label and Song tables" + "\n" + 
+                "4: See a list of the most famous artists, from top to bottom!" + "\n" + 
+                "5: See the sum of all earning of all the recording labels in the database (in 1000$'s)" + "\n" + 
+                "6: All artists with famefactor 5 or higher, starting with the most famous" + "\n" +
+                "Exit: ");
+
+            while (!isValidMenuItem)
+            {
+                string readNum = Console.ReadLine();
+                if (readNum == "1" || readNum == "2" || readNum == "3" || readNum == "4" || readNum == "5" || readNum == "6")
+                {
+                    if(readNum == "1")
+                    {
+                        connect.menuItem1();
+                    }
+                    if (readNum == "2")
+                    {
+                        connect.menuItem2();
+                    }
+                    if (readNum == "3")
+                    {
+                        connect.menuItem3();
+                    }
+                    if (readNum == "4")
+                    {
+                        connect.menuItem4();
+                    }
+                    if (readNum == "5")
+                    {
+                        connect.menuItem5();
+                    }
+                    if (readNum == "6")
+                    {
+                        connect.menuItem6();
+                    }
+                }
+                else if(readNum == "Exit" || readNum == "exit")
+                {
+                    Console.WriteLine("Shutting down the system.");
+                    isValidMenuItem = true;
+                }
+            }
+            
+
+
+        }
+    }
+}
